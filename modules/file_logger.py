20dbdbda81804fd1e04ce1a411cfe616bce7dfd4
David LaJambe <dlajambe@gmail.com>
2024-01-24 21:03:42 -0500

    Add unique log name generation

--- modules/file_logger.py
+++ modules/file_logger.py
@@ -1,7 +1,30 @@
 import os
 import logging
 from logging.handlers import TimedRotatingFileHandler
-from datetime import date
+from datetime import date, datetime
+
+def create_log_name(filename: str) -> str:
+    """Generates a unique filename for a new log file. File names are
+    created with the following format:
+    
+    ml_api_YYYY_MM_DD.log_idx.log
+
+    In the above example, log_idx represents the log number, in case
+    there are already logs with the same prefix in the log directory.
+    """
+    log_dir = os.path.split(filename)[0]
+    today = datetime.now()
+    date_str = today.strftime('%Y_%m_%d')
+
+    log_name_prefix = 'ml_api_' + date_str
+    log_idx = 1
+    log_name = '{}.{}.log'.format(log_name_prefix, str(log_idx))
+    while os.path.exists(os.path.join(log_dir, log_name)):
+        log_idx += 1
+        log_name = '{}.{}.log'.format(log_name_prefix, str(log_idx))
+    
+    filename = os.path.join(log_dir, log_name)
+    return filename
 
 def initialize_file_logger():
     """Initializes and configures the file logger with and log output
@@ -39,11 +62,12 @@ def initialize_file_logger():
         '[%(asctime)s] [%(levelname)s]  %(message)s',
         datefmt='%H:%M:%S')
     log_path = (log_root + year_str +'/' + month_str + '/' 
-                + date_str + 'ml_api.log')
+                + 'ml_api_')
     file_handler = TimedRotatingFileHandler(
             filename=log_path, when='midnight', backupCount=30)
-    file_handler.suffix = '%Y-%m-%d'
+    
     file_handler.setFormatter(log_formatter)
+    file_handler.namer = create_log_name
     
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(log_formatter)
@@ -53,10 +77,8 @@ def initialize_file_logger():
 
     # Step 3 - Format the log output so it contains a time stamp and log
     # log level
-    logging.basicConfig(
-        handlers=handlers, level=logging.DEBUG)
+    logging.basicConfig(handlers=handlers, level=logging.DEBUG)
     logging.info('File logger initialized successfully.')
-
     
 
     
